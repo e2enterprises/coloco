8ca8e42fdced33cf7ed59ebe36928e635aa79a00
Evan Purcer <ep@e2.gg>
2026-08-31 13:54:59 -0700

    v0.1.1

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ Add `coloco` to your list of dependencies in `mix.exs`, then run `mix deps.get`:
 ```elixir
 def deps do
   [
-    {:coloco, "~> 0.1.0"}
+    {:coloco, "~> 0.1.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Coloco.MixProject do
   use Mix.Project
 
   @name "Coloco"
-  @version "0.1.0"
+  @version "0.1.1"
   @repository "https://github.com/e2enterprises/coloco"
 
   defp description() do
